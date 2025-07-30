110317f3213b73fe3070ce0e4ab01302d7070e41
Yusong <yusonglai64@gmail.com>
2025-07-31 07:16:22 +0800

    refactor!: reduce the visibility of some members of lib (#19)
    From `pub` to `pub(crate)`.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@
 pub mod config;
 /// Defines custom error types and their implementations.
 pub mod error;
+/// The entry point of totp_server library.
+pub mod server;
 /// Converts [`tower::Service`] inner errors into [`axum::response::IntoResponse`].
 pub mod service;
 /// Core module for Time-based One-time Password (TOTP).
@@ -13,8 +15,12 @@ pub mod totp;
 /// Utility routers for fallback and health checks.
 pub mod utils;
 
-pub use config::{BIND_PORT, CRATE_NAME, PKG_VERSION, RATE_LIMIT, env_var_check};
+pub(crate) use config::{BIND_PORT, PKG_VERSION, RATE_LIMIT, env_var_check};
+pub(crate) use service::{buffer_error_handler, timeout_error_handler};
+pub(crate) use totp::{VEC_SECRET, check_current};
+pub(crate) use utils::{handler_404, handler_502, health};
+
+pub use config::CRATE_NAME;
 pub use error::{Error, Result};
-pub use service::{buffer_error_handler, timeout_error_handler};
-pub use totp::{InputToken, check_current, try_get_token};
-pub use utils::{handler_404, handler_502, health};
+pub use server::start_server;
+pub use totp::{InputToken, try_get_token};
--- src/utils.rs
+++ src/utils.rs
@@ -2,16 +2,30 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 
 /// Routing fallback.
-pub async fn handler_404() -> impl IntoResponse {
+pub(crate) async fn handler_404() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "404 not found")
 }
 
 /// Routing fallback.
-pub async fn handler_502() -> impl IntoResponse {
+pub(crate) async fn handler_502() -> impl IntoResponse {
     (StatusCode::BAD_GATEWAY, "Not a web service")
 }
 
 /// Health check.
-pub async fn health() -> impl IntoResponse {
+pub(crate) async fn health() -> impl IntoResponse {
     StatusCode::OK
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_handler() {
+        use axum::routing::get;
+        let _ = axum::Router::<()>::new()
+            .route("/", get(handler_502))
+            .route("/health", get(health))
+            .fallback(handler_404);
+    }
 }
