7f52478cdd5087111f3854019ad9cb05ac553839
Yusong <yusonglai64@gmail.com>
2025-08-01 11:34:03 +0800

    feat: use tower_governor to implement rate-limiting (#23)

--- src/server.rs
+++ src/server.rs
@@ -22,9 +22,12 @@ pub async fn start_server() {
         .unwrap_or_else(|e| panic!("failed to bind SocketAddr: {addr}. error: {e}"));
     tracing::info!("listening at http://localhost:{}", addr.port());
 
-    axum::serve(listener, app())
-        .await
-        .unwrap_or_else(|e| panic!("failed to start axum server. error: {e}"));
+    axum::serve(
+        listener,
+        app().into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap_or_else(|e| panic!("failed to start axum server. error: {e}"));
 }
 
 /// Configures and returns the Axum router for the TOTP service.
@@ -39,6 +42,15 @@ pub(crate) fn app() -> axum::Router {
     use std::time::Duration;
     use tower::ServiceBuilder;
 
+    // Configure the rate limiter.
+    let governor_conf = std::sync::Arc::new(
+        tower_governor::governor::GovernorConfigBuilder::default()
+            .per_second(30)
+            .burst_size(*crate::config::RATE_LIMIT)
+            .finish()
+            .expect("failed to configure tower_governor"),
+    );
+
     axum::Router::new()
         .route("/", get(handler_502).post(check_current))
         .route("/health", get(health))
@@ -46,6 +58,9 @@ pub(crate) fn app() -> axum::Router {
         .layer(
             ServiceBuilder::new()
                 .layer(HandleErrorLayer::new(timeout_error_handler))
-                .timeout(Duration::from_secs(1)),
+                .timeout(Duration::from_secs(1))
+                .layer(tower_governor::GovernorLayer {
+                    config: governor_conf,
+                }),
         )
 }
