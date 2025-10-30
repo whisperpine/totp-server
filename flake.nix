97dcbf128283026006d40e05027c4c5b54b6ba12
Yusong <yusonglai64@gmail.com>
2025-10-30 08:25:34 +0800

    chore: bump rust toolchain to the latest nightly version

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
           #   ];
           #   targets = [ "aarch64-unknown-linux-gnu" ];
           # };
-          rust.nightly."2025-06-20".default.override {
+          rust.nightly."2025-10-29".default.override {
             extensions = [
               "rust-src"
               "llvm-tools" # required by cargo-llvm-cov
