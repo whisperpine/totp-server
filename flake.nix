22da50520a1dab7492cd324fefd2eee87aba091e
Yusong <yusonglai64@gmail.com>
2025-11-10 14:22:29 +0800

    chore: print the last modified date of "flake.lock"

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
         { pkgs }:
         {
           default = pkgs.mkShell {
+            # The Nix packages installed in the dev environment.
             packages = with pkgs; [
               rustToolchain
               cargo-edit # managing cargo dependencies
@@ -78,6 +79,13 @@
               hurl # run and test HTTP requests with plain text
               trivy # find vulnerabilities and misconfigurations
             ];
+            # The shell script executed when the environment is activated.
+            shellHook = ''
+              # Print the last modified date of "flake.lock".
+              stat flake.lock | grep "Modify" |
+                awk '{printf "\"flake.lock\" last modified on: %s", $2}' &&
+                echo " ($((($(date +%s) - $(stat -c %Y flake.lock)) / 86400)) days ago)"
+            '';
           };
         }
       );
