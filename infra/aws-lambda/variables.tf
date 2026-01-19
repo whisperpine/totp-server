b9363583a73a33af49350e8be36121d07050c802
Yusong <yusonglai64@gmail.com>
2026-01-20 00:26:05 +0800

    feat(infra): add validation blocks for variables

--- infra/aws-lambda/variables.tf
+++ infra/aws-lambda/variables.tf
@@ -6,8 +6,11 @@ variable "function_name" {
 variable "zip_file_path" {
   description = "The relative file path of bootstrap.zip, built by cargo lambda"
   type        = string
+  validation {
+    condition     = fileexists(var.zip_file_path)
+    error_message = "no such file in ${var.zip_file_path}"
+  }
 }
-
 
 variable "lambda_env_var" {
   description = "the environment variable for aws lambda function"
@@ -18,4 +21,8 @@ variable "lambda_env_var" {
 variable "log_retention_days" {
   description = "the retention days of logs in AWS Cloudwatch"
   type        = number
+  validation {
+    condition     = var.log_retention_days >= 1 && var.log_retention_days <= 180
+    error_message = "the log retention days should be ranged from 1 to 180"
+  }
 }
